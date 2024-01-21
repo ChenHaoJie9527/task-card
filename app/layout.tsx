f1fad57943cfc5e935050b7c86e118b70f77b377
ChenHaoJie9527 <a18520145493@163.com>
2024-01-21 22:29:00 +0800

    fix: 修复ClerkProvider多次重复注册报错问题

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { siteConfig } from "@/config";
 import "./globals.css";
-import PlatformLayout from "./(platform)/layout";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,10 +26,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <PlatformLayout>
+    <ClerkProvider>
+      <html lang="en">
         <body className={inter.className}>{children}</body>
-      </PlatformLayout>
-    </html>
+      </html>
+    </ClerkProvider>
   );
 }
