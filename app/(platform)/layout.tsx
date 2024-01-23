b26e8556d4df64392b13df6089ac7f6fca13a571
ChenHaoJie9527 <a18520145493@163.com>
2024-01-23 17:05:02 +0800

    feat: Navbar行内间距调整

--- app/(platform)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import { ReactNode } from "react";
-export default function PlatformLayout({ children }: { children: ReactNode }) {
-  return <ClerkProvider>{children}</ClerkProvider>;
-}
