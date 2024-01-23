f27c94ab7b37e27029ea4e9883fd18ee0abbd880
ChenHaoJie9527 <a18520145493@163.com>
2024-01-23 17:44:42 +0800

    feat: organization layout-Sidebar布局

--- app/(dashboard)/organization/layout.tsx
+++ app/(dashboard)/organization/layout.tsx
@@ -7,7 +7,11 @@ export default function OrganizationPageLayout({
 }) {
   return (
     <main className="pt-20 md:pt-24 px-4 max-w-7xl 2xl:max-w-screen-xl mx-auto">
-      {children}
+      <div className="flex gap-x-7">
+        {/* TODO: Sidebar */}
+        <div className="w-64 shrink-0 hidden md:block"></div>
+        {children}
+      </div>
     </main>
   );
 }
