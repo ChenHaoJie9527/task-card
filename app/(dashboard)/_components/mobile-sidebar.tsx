f38db8d2e89d33dd24be7a2f94ff9a723d167c53
ChenHaoJie9527 <a18520145493@163.com>
2024-01-24 16:56:49 +0800

    feat: 监听pathName 隐藏mobile-sidebar

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -2,17 +2,30 @@
 
 import { useMobileSidebarStore } from "@/hooks/use-mobile-sidebar";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 const MobileSidebar = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
   const { onOpen, isOpen, onClose } = useMobileSidebarStore();
+  const [mounted, setMounted] = useState(false);
+  const pathName = usePathname();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    onClose();
+  }, [pathName, onClose]);
+
+  if (!mounted) {
+    return null;
+  }
   return (
-    <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
-      {...props}
-    >
+    <div className={cn("rounded-md bg-muted", className)} {...props}>
       MobileSidebar
     </div>
   );
