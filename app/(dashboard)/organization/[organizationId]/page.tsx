6399d0d112e115d0f928e9e44c41a9a9cfbf49ad
ChenHaoJie9527 <a18520145493@163.com>
2024-01-26 09:48:18 +0800

    feat: 查询 board 表里的所有数据

--- app/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,11 @@
 import { create } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
+import { db } from "@/db";
 
-export default function OrganizationPage({ params }: { params: any }) {
+export default async function OrganizationPage({ params }: { params: any }) {
+  const boardList = await db.board.findMany({
+    where: {},
+  });
   return (
     <form action={create}>
       <input
@@ -15,6 +19,12 @@ export default function OrganizationPage({ params }: { params: any }) {
       <Button type="submit" size="sm">
         submit
       </Button>
+      {boardList.map((board) => (
+        <div key={board.id} className="flex items-center justify-start">
+          <span>{board.id}: </span>
+          <span>{board.title}</span>
+        </div>
+      ))}
     </form>
   );
 }
