cc98585fec14e41a4ef8e5699c21e6cabc425b9c
ChenHaoJie9527 <a18520145493@163.com>
2024-01-26 08:55:57 +0800

    feat: create db board in to title

--- app/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,27 @@
+import { db } from "@/db";
+
 export default function OrganizationPage({ params }: { params: any }) {
-  return <div>123</div>;
+  const action = async (form: FormData) => {
+    "use server";
+
+    const title = form.get("title") as string;
+    // TODO: create board in to title
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  };
+  return (
+    <form action={action}>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        required
+        className="border"
+        placeholder="pleas title"
+      />
+    </form>
+  );
 }
